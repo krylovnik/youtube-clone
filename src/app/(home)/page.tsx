b217e424cc59c38cf5ff1960c48680fa0438e964
Krylov Mykyta <krylov.nik33@gmail.com>
2025-08-20 21:38:42 +0200

    added schema for categories, prefetched categories data, updated structure for the project

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,21 @@
-import {trpc} from "@/trpc/server"
+import {HydrateClient, trpc} from "@/trpc/server"
+import {HomeView} from "@/modules/home/ui/views/home-view";
 
-export default async function Home() {
-    const data = await trpc.hello({text: "Me"})
+export const dynamic = "force-dynamic";
+
+interface PageProps {
+    searchParams: Promise<{
+        categoryId?: string;
+    }>
+}
+
+const Page = async ({searchParams}: PageProps)=> {
+    const {categoryId} = await searchParams;
+    void trpc.categories.getMany.prefetch()
     return (
-        <div>
-            Client component says: {data.greeting}
-        </div>
+        <HydrateClient>
+            <HomeView categoryId={categoryId}/>
+        </HydrateClient>
     );
 }
+export default Page;
