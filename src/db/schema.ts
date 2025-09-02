657d9b64670f26ffbab75c64e56ba79698f5b7f6
Krylov Mykyta <krylov.nik33@gmail.com>
2025-09-02 21:17:07 +0200

    fixed single video data prefetch, added form using react hook form

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,5 +1,6 @@
 import {integer, pgEnum, pgTable, text, timestamp, uniqueIndex, uuid} from "drizzle-orm/pg-core";
 import {relations} from "drizzle-orm";
+import {createInsertSchema, createSelectSchema, createUpdateSchema} from "drizzle-zod";
 
 export const users = pgTable("users", {
     id: uuid("id").primaryKey().defaultRandom(),
@@ -39,6 +40,10 @@ export const videos = pgTable("videos", {
     createdAt: timestamp("created_at").defaultNow().notNull(),
     updatedAt: timestamp("updated_at").defaultNow().notNull(),
 })
+
+export const videoInsertSchema = createInsertSchema(videos)
+export const videoUpdateSchema = createUpdateSchema(videos)
+export const videoSelectSchema = createSelectSchema(videos)
 
 export const videoRelations = relations(videos, ({ one }) => ({
     user: one(users, {
---
+++ src/modules/studio/sections/form-section.tsx
@@ -0,0 +1,75 @@
+"use client"
+
+
+import {trpc} from "@/trpc/client";
+import {Suspense} from "react";
+import {ErrorBoundary} from "react-error-boundary";
+import {Button} from "@/components/ui/button";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import {MoreVerticalIcon, TrashIcon} from "lucide-react";
+import {useForm} from "react-hook-form";
+import {videoUpdateSchema} from "@/db/schema";
+import {zodResolver} from "@hookform/resolvers/zod";
+import z from "zod";
+import {Form} from "@/components/ui/form";
+
+interface FormSectionProps {
+    videoId: string
+}
+
+export const FormSection = ({videoId}: FormSectionProps) => {
+    return (
+        <Suspense fallback={<FormSectionSkeleton/>}>
+            <ErrorBoundary fallback={<p>Error</p>}>
+                <FormSectionSuspense videoId={videoId}/>
+            </ErrorBoundary>
+        </Suspense>
+    )
+}
+const FormSectionSkeleton = () => {
+   return  <p>Loading...</p>
+}
+
+const FormSectionSuspense = ({videoId}: FormSectionProps) => {
+    const [video] = trpc.studio.getOne.useSuspenseQuery({id: videoId})
+
+    const form = useForm<z.infer<typeof videoUpdateSchema>>({
+        resolver: zodResolver(videoUpdateSchema),
+        defaultValues: video,
+    })
+    const onSubmit = async (data: z.infer<typeof videoUpdateSchema>) => {
+        console.log(data)
+    }
+
+
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+                <div className="flex items-center justify-between mb-6">
+                    <div>
+                        <h1 className="text-2xl font-bold">Video details</h1>
+                        <p className="text-xs text-muted-foreground">Manage your video details</p>
+                    </div>
+                    <div className="flex items-center gap-x-2">
+                        <Button type="submit" disabled={false}>
+                            Save
+                        </Button>
+                        <DropdownMenu>
+                            <DropdownMenuTrigger asChild>
+                                <Button variant="ghost" size="icon">
+                                    <MoreVerticalIcon/>
+                                </Button>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent align="end">
+                                <DropdownMenuItem>
+                                    <TrashIcon className="size-4 mr-2"/>
+                                    Delete
+                                </DropdownMenuItem>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
+                    </div>
+                </div>
+            </form>
+        </Form>
+    )
+}
