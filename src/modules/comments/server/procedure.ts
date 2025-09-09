079d06b5e115dd107b2e6e087306d608dd44e669
Krylov Mykyta <krylov.nik33@gmail.com>
2025-09-09 15:07:40 +0200

    added pagination and infinite load for comments

--- src/modules/comments/server/procedure.ts
+++ src/modules/comments/server/procedure.ts
@@ -2,33 +2,90 @@ import {baseProcedure, createTRPCRouter, protectedProcedure} from "@/trpc/init";
 import {z} from "zod";
 import {db} from "@/db";
 import {comments, users} from "@/db/schema";
-import {eq, getTableColumns} from "drizzle-orm";
+import {and, count, desc, eq, getTableColumns, lt, or} from "drizzle-orm";
+import {TRPCError} from "@trpc/server";
 
 export const commentsRouter = createTRPCRouter({
     create: protectedProcedure
         .input(z.object({videoId: z.uuid(), value: z.string()}))
-        .mutation(async ({input ,ctx}) => {
+        .mutation(async ({input, ctx}) => {
             const {videoId, value} = input;
             const {id: userId} = ctx.user;
 
-           const [createdComment] = await db
-               .insert(comments)
-               .values({userId ,videoId, value})
-               .returning();
-           return createdComment
+            const [createdComment] = await db
+                .insert(comments)
+                .values({userId, videoId, value})
+                .returning();
+            return createdComment
+        }),
+    remove: protectedProcedure
+        .input(z.object({id: z.uuid()}))
+        .mutation(async ({input, ctx}) => {
+            const {id} = input;
+            const {id: userId} = ctx.user;
+
+            const [deletedComment] = await db
+                .delete(comments)
+                .where(and(
+                    eq(comments.id, id),
+                    eq(comments.userId, userId)
+                ))
+                .returning();
+            if (!deletedComment) {
+                throw new TRPCError({code: "NOT_FOUND"});
+            }
+
+            return deletedComment
         }),
     getMany: baseProcedure
-        .input(z.object({videoId: z.uuid()}))
+        .input(z.object({
+            videoId: z.uuid(),
+            cursor: z.object({id: z.uuid(), updatedAt: z.date()}).nullish(),
+            limit: z.number().min(1).max(100)
+        }))
         .query(async ({input}) => {
-            const {videoId} = input;
-            const data = await db
-                .select({
-                    ...getTableColumns(comments),
-                    user: users
-                })
-                .from(comments)
-                .where(eq(comments.videoId, videoId))
-                .innerJoin(users, eq(comments.userId, users.id))
-            return data;
+            const {videoId, cursor, limit} = input;
+
+            const [totalData, data] = await Promise.all([
+                await db
+                    .select({
+                        count: count()
+                    })
+                    .from(comments)
+                    .where(eq(comments.videoId, videoId)),
+
+                await db
+                    .select({
+                        ...getTableColumns(comments),
+                        user: users,
+                    })
+                    .from(comments)
+                    .where(and(
+                        eq(comments.videoId, videoId),
+                        cursor ? or(lt(comments.updatedAt, cursor.updatedAt),
+                            and(
+                                eq(comments.updatedAt, cursor.updatedAt),
+                                lt(comments.id, cursor.id)
+                            )
+                        ) : undefined
+                    ))
+                    .innerJoin(users, eq(comments.userId, users.id))
+                    .orderBy(desc(comments.updatedAt), desc(comments.id))
+                    .limit(limit + 1)
+            ])
+
+
+            const hasMore = data.length > limit;
+            const items = hasMore ? data.slice(0, -1) : data;
+            const lastItem = items[items.length - 1];
+            const nextCursor = hasMore ? {
+                id: lastItem.id,
+                updatedAt: lastItem.updatedAt,
+            } : null
+            return {
+                totalCount: totalData[0].count,
+                items,
+                nextCursor
+            }
         })
 })
