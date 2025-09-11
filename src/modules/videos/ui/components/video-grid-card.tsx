a83ece3721651e8c10695d97f9af1309a65492ca
Krylov Mykyta <krylov.nik33@gmail.com>
2025-09-11 17:04:48 +0200

    added suggestions section

---
+++ src/modules/videos/ui/components/video-grid-card.tsx
@@ -0,0 +1,25 @@
+import {VideoGetManyOutput} from "@/modules/videos/types";
+import Link from "next/link";
+import {VideoThumbnail} from "@/modules/videos/ui/components/video-thumbnail";
+import {VideoInfo} from "@/modules/videos/ui/components/video-info";
+
+interface VideoGridCardProps {
+    data: VideoGetManyOutput["items"][number];
+    onRemove?: () => void;
+}
+
+export const VideoGridCard = ({data,onRemove}: VideoGridCardProps) => {
+    return (
+        <div className="flex flex-col gap-2 w-full group">
+            <Link href={`/videos/${data.id}`}>
+                <VideoThumbnail
+                    imageUrl={data.thumbnailUrl}
+                    previewUrl={data.previewUrl}
+                    title={data.title}
+                    duration={data.duration ?? 0}
+                />
+            </Link>
+            <VideoInfo data={data} onRemove={onRemove} />
+        </div>
+    )
+}
--- src/modules/videos/ui/sections/suggestions-section.tsx
+++ src/modules/videos/ui/sections/suggestions-section.tsx
@@ -1,6 +1,54 @@
-export const SuggestionsSection = () => {
+"use client"
+
+import {trpc} from "@/trpc/client";
+import {DEFAULT_LIMIT} from "@/constants";
+import {VideoRowCard} from "@/modules/videos/ui/components/video-row-card";
+import {VideoGridCard} from "@/modules/videos/ui/components/video-grid-card";
+import {InfiniteScroll} from "@/components/infinite-scroll";
+
+interface SuggestionsSectionProps {
+    videoId: string;
+    isManual?: boolean;
+}
+
+
+export const SuggestionsSection = ({videoId,isManual}: SuggestionsSectionProps) => {
+    const [suggestions, query] = trpc.suggestions.getMany.useSuspenseInfiniteQuery({
+            videoId: videoId,
+            limit: DEFAULT_LIMIT
+        },
+        {
+            getNextPageParam: (lastPage) => lastPage.nextCursor
+        })
+
+
     return (
-        <div>Suggestions</div>
+        <>
+            <div className="hidden md:block space-y-3">
+                {suggestions.pages.flatMap((page) => page.items.map((video) => (
+                    <VideoRowCard
+                        key={video.id}
+                        data={video}
+                        size="compact"
+                    />
+                )))}
+            </div>
+            <div className="block md:hidden space-y-10">
+                {suggestions.pages.flatMap((page) => page.items.map((video) => (
+                    <VideoGridCard
+                        key={video.id}
+                        data={video}
+                    />
+                )))}
+            </div>
+            <InfiniteScroll
+                isManual={isManual}
+                hasNextPage={query.hasNextPage}
+                isFetchingNextPage={query.isFetchingNextPage}
+                fetchNextPage={query.fetchNextPage}
+            />
+        </>
+
     )
 
 }
